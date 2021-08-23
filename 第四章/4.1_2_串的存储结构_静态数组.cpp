f5cee5a70639b90f4f85763bf8882362ba25f504
yuexiliuli <1614639905@qq.com>
2021-08-23 20:23:03 +0800

    4.1_2_串的存储结构

--- 第四章/4.1_2_串的存储结构_静态数组.cpp
+++ 第四章/4.1_2_串的存储结构_静态数组.cpp
@@ -13,6 +13,35 @@ typedef struct
 bool SubString(SString &Sub,SString S,int pos,int len){
     //子串范围越界
     if(pos+len-1>S.length){
-        
+        return false;
     }
+    for(int i=pos;i<pos+len;i++){
+        Sub.ch[i-pos+1] = S.ch[i];
+    }
+    Sub.length = len;
+    return true;
 }
+//比较两个串的大小,若S>T,返回值>0;若S=T,则返回值=0;若S<T,则返回值<0
+int StrCompare(SString S,SString T){
+    for(int i = 1;i<S.length && i<T.length ;i++){
+        if(S.ch[i]!=T.ch[i])
+            return S.ch[i]-T.ch[i];
+    }
+    //扫描过的所有字符都相同，则长度长的串更大
+    return S.length - T.length;
+}
+
+//定位操作,若主串S中存在与串T值相同的子串，则返回它在主串中第一次出现的位置
+int Index(SString S, SString T){
+    int i=1,n=S.length,m=T.length;
+    SString sub;  //用于暂存串
+    while(i<=n-m+1){
+        SubString(sub,S,i,m);
+        if(StrCompare(sub,T)!=0)++i;
+        else return i; //返回子串在主串中的位置
+    }
+    return 0;   //S中不存在与T相等的的子串
+}
+
+
+
